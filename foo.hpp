39d8fb5deda27e0f66284c7bbef1b3b6902b94d5
jakubasz <92277382+jakubasz@users.noreply.github.com>
2022-12-20 23:47:58 +0100

    Update foo.hpp

--- foo.hpp
+++ foo.hpp
@@ -11,6 +11,10 @@ std::vector< char > foo(std::list< Human >& people)
     std::vector< char > ret_v(people.size());
 
     // Twoja implementacja tutaj
+std::vector< char > v(people.size());
 
+	std::for_each(people.begin(), people.end(), [](Human &n) { n.birthday(); });
+	std::transform(people.begin(), people.end(), v.begin(), [](const Human &n) { return n.isMonster() ? 'n' : 'y'; });
+	std::reverse(v.begin(), v.end());
     return ret_v;
 }
